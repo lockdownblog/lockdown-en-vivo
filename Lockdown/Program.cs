4e20ab8cc75d1974e9966f2e88736148c08c72b2
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-01-09 17:22:17 +0000

    Mejorando el código

--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
-namespace Lockdown
+﻿namespace Lockdown
 {
-    class Program
+    using System;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
