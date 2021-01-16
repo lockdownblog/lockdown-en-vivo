54ce3bf60a9ef713b3e29d3cc43618ca4bb841f7
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-01-16 17:07:47 +0000

    Agregué una calculadora

--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Lockdown
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
     using System.IO;
     using McMaster.Extensions.CommandLineUtils;
 
@@ -10,6 +11,18 @@
     {
         public string LockdownVersion { get; } = "0.0.0";
 
+        [Argument(0, Description = "The first operand")]
+        [Required]
+        public int FirstNumber { get; set; }
+
+        [Argument(1, Description = "Operation to perform")]
+        [Required]
+        public string Operation { get; set; }
+
+        [Argument(2, Description = "The second operand")]
+        [Required]
+        public int SecondNumber { get; set; }
+
         public static int Main(string[] args)
         {
             return CommandLineApplication.Execute<Program>(args);
@@ -17,7 +30,27 @@
 
         public int OnExecute(CommandLineApplication app)
         {
-            app.ShowHelp();
+            int result;
+            switch (this.Operation)
+            {
+                case "+":
+                    result = this.FirstNumber + this.SecondNumber;
+                    break;
+                case "-":
+                    result = this.FirstNumber - this.SecondNumber;
+                    break;
+                case "/":
+                    result = this.FirstNumber / this.SecondNumber;
+                    break;
+                case "*":
+                    result = this.FirstNumber * this.SecondNumber;
+                    break;
+                default:
+                    throw new NotImplementedException($"The operation {this.Operation} is not implemented");
+            }
+
+            Console.WriteLine(result);
+
             return 0;
         }
     }
