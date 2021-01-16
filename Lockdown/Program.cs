53475e6fabd2224f8eab9d5e89791eeb1c8bdc7a
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-01-16 16:36:20 +0000

    Agregué código ejemplo

--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -1,12 +1,16 @@
 ﻿namespace Lockdown
 {
     using System;
+    using System.IO;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var argumentos = string.Join("; ", args);
+            var directorioActual = Directory.GetCurrentDirectory();
+            Console.WriteLine($"Me estoy ejecutando en {directorioActual}");
+            Console.WriteLine(argumentos);
         }
     }
 }
