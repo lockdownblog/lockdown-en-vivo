5a5103b3f3be522e0f0b3afb4b2378bda45af12f
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-01-16 17:26:13 +0000

    Agregué subcomando build

---
+++ Lockdown/Commands/BuildCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Lockdown.Commands
+{
+    using System;
+
+    public class BuildCommand
+    {
+        public int OnExecute()
+        {
+            Console.WriteLine("You executed the build command");
+            return 0;
+        }
+    }
+}
--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -1,9 +1,11 @@
 ﻿namespace Lockdown
 {
+    using Lockdown.Commands;
     using McMaster.Extensions.CommandLineUtils;
 
     [Command("lockdown")]
     [VersionOptionFromMember("--version", MemberName = nameof(LockdownVersion))]
+    [Subcommand(typeof(BuildCommand))]
     public class Program
     {
         public string LockdownVersion { get; } = "0.0.0";
