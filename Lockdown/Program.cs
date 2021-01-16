dcd7becb70f0c76089581e30d13ae4b8cd8fba18
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-01-16 17:12:48 +0000

    Borré mi calculadora

--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -1,8 +1,5 @@
 ﻿namespace Lockdown
 {
-    using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.IO;
     using McMaster.Extensions.CommandLineUtils;
 
     [Command("lockdown")]
@@ -11,18 +8,6 @@
     {
         public string LockdownVersion { get; } = "0.0.0";
 
-        [Argument(0, Description = "The first operand")]
-        [Required]
-        public int FirstNumber { get; set; }
-
-        [Argument(1, Description = "Operation to perform")]
-        [Required]
-        public string Operation { get; set; }
-
-        [Argument(2, Description = "The second operand")]
-        [Required]
-        public int SecondNumber { get; set; }
-
         public static int Main(string[] args)
         {
             return CommandLineApplication.Execute<Program>(args);
@@ -30,27 +15,7 @@
 
         public int OnExecute(CommandLineApplication app)
         {
-            int result;
-            switch (this.Operation)
-            {
-                case "+":
-                    result = this.FirstNumber + this.SecondNumber;
-                    break;
-                case "-":
-                    result = this.FirstNumber - this.SecondNumber;
-                    break;
-                case "/":
-                    result = this.FirstNumber / this.SecondNumber;
-                    break;
-                case "*":
-                    result = this.FirstNumber * this.SecondNumber;
-                    break;
-                default:
-                    throw new NotImplementedException($"The operation {this.Operation} is not implemented");
-            }
-
-            Console.WriteLine(result);
-
+            app.ShowHelp();
             return 0;
         }
     }
