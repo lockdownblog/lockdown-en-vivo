8a4afb5c5f0f8f3642b95cd8e8b6b7f123756d18
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-01-23 17:51:43 +0000

    Bump version: 0.0.2 → 0.0.3

--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -9,7 +9,7 @@ namespace Lockdown
     [Subcommand(typeof(BuildCommand))]
     public class Program
     {
-        public string LockdownVersion { get; } = "0.0.2";
+        public string LockdownVersion { get; } = "0.0.3";
 
         public static int Main(string[] args)
         {
