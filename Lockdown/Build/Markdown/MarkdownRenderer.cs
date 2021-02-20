84c6814d1f81c75e410b11be3f3bf7e87c7555ef
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-02-20 12:04:32 +0000

    Add markdown renderer

---
+++ Lockdown/Build/Markdown/MarkdownRenderer.cs
@@ -0,0 +1,24 @@
+﻿namespace Lockdown.Build.Markdown
+{
+    using System.IO;
+    using Markdig;
+    using Markdig.Renderers;
+
+    public class MarkdownRenderer : IMarkdownRenderer
+    {
+        public string RenderMarkdown(string text)
+        {
+            var document = Markdown.Parse(text);
+
+            using var writer = new StringWriter();
+            var htmlRenderer = new HtmlRenderer(writer);
+
+            foreach (var documentPart in document)
+            {
+                htmlRenderer.Write(documentPart);
+            }
+
+            return writer.ToString();
+        }
+    }
+}
